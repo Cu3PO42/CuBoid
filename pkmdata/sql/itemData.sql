817bc9525ce234bee7bdfed27d6150680dae2a17
Copper Phosphate <cu3po42@gmail.com>
2015-07-07 13:12:20 +0200

    $item hotfix.

--- pkmdata/sql/itemData.sql
+++ pkmdata/sql/itemData.sql
@@ -4,6 +4,7 @@ FROM item_prose
 JOIN item_names ON item_prose.item_id = item_names.item_id
 AND item_names.local_language_id = 9
 WHERE item_prose.item_id =
-        (SELECT item_id
+        (SELECT DISTINCT item_id
          FROM item_names
          WHERE name = ?)
+AND item_prose.local_language_id = 9
