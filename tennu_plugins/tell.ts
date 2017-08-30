64035a6b00b3ab6b5cc4354f212e845699adf201
Copper Phosphate <cu3po42@gmail.com>
2017-08-30 18:21:56 +0200

    Migrate $tell to new SQL code

--- tennu_plugins/tell.ts
+++ tennu_plugins/tell.ts
@@ -1,4 +1,4 @@
-import mysql from "../mysql-bootstrap";
+import { createPool } from "mysql";
 import * as moment from "moment";
 import * as _ from "lodash";
 import * as util from "util";
@@ -30,12 +30,24 @@ function defer<T>() {
 }
 
 export function init(client: Tennu.Client, imports: Tennu.PluginImports) {
-    var pool = mysql.createPool(client.config("tell-database")),
+    var pool = createPool(client.config("tell-database")),
         messageExtractor = /.tell\s*\S+\s*(.*)$/;
 
     client._socket.on("close", () => {
         pool.end(_.noop);
     });
+
+    async function execSql(statement: string, ...params: string[]) {
+        return await new Promise((resolve, reject) => {
+            pool.getConnection((err, conn) => {
+                if (err) reject(err);
+                conn.query(statement, params, (err, res) => {
+                    if (err) reject(err);
+                    resolve(res);
+                });
+            })
+        }) as Promise<any[]>;
+    }
 
     var registeredResolvers: { [nick: string]: Resolver<boolean> } = {},
         nickserv: string = client.config("nickserv").toLowerCase();
@@ -79,15 +91,14 @@ export function init(client: Tennu.Client, imports: Tennu.PluginImports) {
                 if (user.value.identified) {
                     var message = command.message.match(messageExtractor)[1];
                     const registered = await isRegistered(command.args[0]);
-                    if (registered) {
-                        return true;
+                    if (!registered) {
+                        client.say(command.nickname, [util.format("The user '%s' is not registered. Are you sure you want to send this message, %s?", command.args[0], command.nickname), "Please use $confirmtell to confirm or $aborttell to abort."]);
+                        if (!await confirmTell(command.nickname)) {
+                            return "Aborted.";
+                        }
                     }
-                    client.say(command.nickname, [util.format("The user '%s' is not registered. Are you sure you want to send this message, %s?", command.args[0], command.nickname), "Please use $confirmtell to confirm or $aborttell to abort."]);
-                    if (await confirmTell(command.nickname)) {
-                        pool.execSql("INSERT INTO messages (from_user, to_user, message) VALUES (?, ?, ?)", [user.value.identifiedas, command.args[0], message]);
-                        return "I'll pass that on."
-                    }
-                    return "Aborted.";
+                    execSql("INSERT INTO messages (from_user, to_user, message) VALUES (?, ?, ?)", user.value.identifiedas, command.args[0], message);
+                    return "I'll pass that on."
                 }
                 return "You need to be identified to use this service."
             },
@@ -130,13 +141,13 @@ export function init(client: Tennu.Client, imports: Tennu.PluginImports) {
             },
 
             privmsg: (message: Tennu.MessagePrivmsg) => {
-                pool.execSql("SELECT * FROM messages WHERE to_user = ?", [message.nickname])
+                execSql("SELECT * FROM messages WHERE to_user = ?", message.nickname)
                 .then((messages: StoredMessage[]) => {
                     if (messages.length) {
                         confirmReceive(message.nickname)
                         .then((ok) => {
                             if (ok) {
-                                pool.execSql("DELETE FROM messages WHERE to_user = ?", [message.nickname]);
+                                execSql("DELETE FROM messages WHERE to_user = ?", message.nickname);
                                 client.say(message.nickname, _.map(messages, (e) => { return util.format("%s: %s said %s to tell you: %s", message.nickname, e.from_user, moment(e.time).fromNow(), e.message); }))
                             }
                         })
